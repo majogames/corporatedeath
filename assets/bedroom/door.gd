a9492c99f88a6ecdc4f4d64dfeb1d22155c2888c
Matteo De Carlo <matteo.dek@covolunablu.org>
2022-05-24 00:35:30 +0200

    activate door also with E key

--- assets/bedroom/door.gd
+++ assets/bedroom/door.gd
@@ -5,7 +5,7 @@ var door_open = false
 
 func _input(event):
 	
-	if Input.is_action_just_pressed("left_mouse_click"):
+	if Input.is_action_just_pressed("action"):
 		if player_infront == true and $AnimationPlayer.is_playing() == false:
 			door_open = !door_open
 			if door_open == true:
