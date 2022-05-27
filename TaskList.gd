767051dec7f9588a61217b3e83316b7288499a7e
Matteo De Carlo <matteo.dek@covolunablu.org>
2022-05-27 23:51:20 +0200

    Blocked door until you finish morning tasks

--- TaskList.gd
+++ TaskList.gd
@@ -1,4 +1,8 @@
 extends Node
 
 
+enum DayTime {Morning=0, MorningWork=1, AfternoonWork=2, Evening=3, Night=4, MAX=5}
+
 var dead = false
+var daytime = DayTime.Morning
+
--- assets/bedroom/door.gd
+++ assets/bedroom/door.gd
@@ -1,8 +1,14 @@
 extends Spatial
 
 var door_open = false
+export var enabled = false
 
 func toggle_door():
+	if !enabled:
+		return
+	if TaskList.daytime == TaskList.DayTime.Morning:
+		print("TODO text: first I have to complete my morning tasks")
+		return
 	door_open = !door_open
 	if door_open == true:
 		$AnimationPlayer.play("DoorOpen")
