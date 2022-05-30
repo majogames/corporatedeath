3cbd2d84a82e3af85354838ca86c2e6cff3eab68
Matteo De Carlo <matteo.dek@covolunablu.org>
2022-05-30 15:19:06 +0200

    Finally first day ended

---
+++ assets/bedroom/Phone Charger.gd
@@ -0,0 +1,14 @@
+extends MeshInstance
+
+var dialog = null
+
+func action():
+	if is_instance_valid(dialog):
+		return
+	dialog = Dialogic.start('charge phone')
+	add_child(dialog)
+	yield(dialog, "timeline_end")
+	var phone_charging = Dialogic.get_variable("task_charge_phone", 'false')
+	print("ALLLORAAAA ", phone_charging)
+	if phone_charging == 'true':
+		$phone.visible = true
