67045a6934c3996df2db1b62312c4493e0ef2b2f
Bezelbee <abezelbee@gmail.com>
2022-05-30 22:10:34 +0200

    the cats are very broken

--- assets/cat/cat_origami.gd
+++ assets/cat/cat_origami.gd
@@ -1,5 +1,11 @@
 extends Spatial
 
+export var cat_name = 'none'
 
 func action():
+	if get_tree().get_current_scene().get_name() == "Forest":
+		print(cat_name)
+		print("bring me home!!")
+	else:
+		print("purr")
 	pass
